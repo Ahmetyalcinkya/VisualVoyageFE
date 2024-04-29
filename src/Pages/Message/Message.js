04cc805afd9b522f538f80637a0b0469309e1f4b
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-29 14:19:12 +0300

    message part implemented!

--- src/Pages/Message/Message.js
+++ src/Pages/Message/Message.js
@@ -1,8 +1,15 @@
-import { Grid, IconButton } from "@mui/material";
+import { Avatar, Grid, IconButton } from "@mui/material";
 import React from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import AddIcCallIcon from "@mui/icons-material/AddIcCall";
+import VideoCallIcon from "@mui/icons-material/VideoCall";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 
 const Message = () => {
+  const imageSelectHandler = () => {
+    console.log("image select");
+  };
+
   return (
     <div>
       <Grid container className="h-screen overflow-y-hidden">
@@ -24,7 +31,52 @@ const Message = () => {
             </div>
           </div>
         </Grid>
-        <Grid item xs={9}></Grid>
+        <Grid className="h-full" item xs={9}>
+          <div>
+            <div className="flex justify-between items-center border-l p-5">
+              <div className="flex items-center space-x-3">
+                <Avatar />
+                {/* Messaging user profile picture will be here! */}
+                <p>User first name and last name</p>
+                {/* User full name will be here! */}
+              </div>
+              <div className="flex space-x-3">
+                <IconButton>
+                  <AddIcCallIcon />
+                </IconButton>
+                <IconButton>
+                  <VideoCallIcon />
+                </IconButton>
+              </div>
+            </div>
+            <div className="hideScrollbar overflow-y-scroll h-[82vh] px-2 space-y-5 py-5">
+              Messages will be here!
+            </div>
+          </div>
+          <div className="sticky bottom-0 border-l">
+            <div className="py-5 flex items-center justify-center space-x-5 px-4">
+              <Avatar />
+              {/* Auth user profile picture will be here! */}
+              <input
+                type="text"
+                className="bg-transparent border border-[#3b4054] rounded-full w-[90%] py-3 px-5"
+                placeholder="Type message..."
+              />
+              <div>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={imageSelectHandler}
+                  className="hidden"
+                  id="image-input"
+                />
+                <label htmlFor="image-input">
+                  <AddPhotoAlternateIcon />
+                </label>
+              </div>
+            </div>
+          </div>
+        </Grid>
       </Grid>
     </div>
   );
